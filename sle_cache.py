dac119501fdd4de6ef7343ac61cc23b1badedc5b
ojeme <ojeme@mojjo.fr>
2016-11-21 17:57:30 +0100

    end of tp

--- sle_cache.py
+++ sle_cache.py
@@ -13,23 +13,23 @@ expe.cache.size=16*1024
 expe.cache.associativity=64
 
 expe.array_size=[512,512]
-expe.tile_max=8
-expe.tile_step=4
+expe.tile_max=128
+expe.tile_step=16
 # lance experience avec angle de rotation 0
-expe.app.alpha=0.0
-expe.plot_name='res/expe_simple.pdf'
+expe.app.alpha=1.57
+expe.plot_name='res/expe_simple0.52.pdf'
 expe.run()
 expe.plot()
 print('done')
-expe.app.alpha=0.52
-expe.plot_name='res/e2.pdf'
-expe.run()
-expe.plot()
-print('done')
+#expe.app.alpha=0.52
+#expe.plot_name='res/e2.pdf'
+#expe.run()
+#expe.plot()
+#print('done')
 # Exemple pour OBL
-expe.idx=cache.Array_ref_OBL()
-expe.idx.OBL_tile=[32, 2]
-expe.dta='res/e1_OBL.pdf'
-expe.run()
-expe.plot()
-print('done')
+#expe.idx=cache.Array_ref_OBL()
+#expe.idx.OBL_tile=[32, 2]
+#expe.dta='res/e1_OBL.pdf'
+#expe.run()
+#expe.plot()
+#print('done')
